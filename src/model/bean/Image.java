ec95b5feb427b4900372c8b3459658424d484f66
NguyenDucThuan <spectre1451991@gmail.com>
2017-11-26 18:06:33 +0700

    + Create servlet PublicShowListCanHo

---
+++ src/model/bean/Image.java
@@ -0,0 +1,33 @@
+package model.bean;
+
+public class Image {
+	private int idHinhAnh;
+	private String hinhAnh;
+	private int idCanHo;
+	public int getIdHinhAnh() {
+		return idHinhAnh;
+	}
+	public void setIdHinhAnh(int idHinhAnh) {
+		this.idHinhAnh = idHinhAnh;
+	}
+	public String getHinhAnh() {
+		return hinhAnh;
+	}
+	public void setHinhAnh(String hinhAnh) {
+		this.hinhAnh = hinhAnh;
+	}
+	public int getIdCanHo() {
+		return idCanHo;
+	}
+	public void setIdCanHo(int idCanHo) {
+		this.idCanHo = idCanHo;
+	}
+	public Image(int idHinhAnh, String hinhAnh, int idCanHo) {
+		super();
+		this.idHinhAnh = idHinhAnh;
+		this.hinhAnh = hinhAnh;
+		this.idCanHo = idCanHo;
+	}
+	
+	
+}
---
+++ src/model/bean/TienIch.java
@@ -0,0 +1,24 @@
+package model.bean;
+
+public class TienIch {
+	private int idTienIch;
+	private String tenTienIch;
+	public int getIdTienIch() {
+		return idTienIch;
+	}
+	public void setIdTienIch(int idTienIch) {
+		this.idTienIch = idTienIch;
+	}
+	public String getTenTienIch() {
+		return tenTienIch;
+	}
+	public void setTenTienIch(String tenTienIch) {
+		this.tenTienIch = tenTienIch;
+	}
+	public TienIch(int idTienIch, String tenTienIch) {
+		super();
+		this.idTienIch = idTienIch;
+		this.tenTienIch = tenTienIch;
+	}
+}	
+	
